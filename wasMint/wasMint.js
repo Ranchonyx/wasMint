691f998026c51cdef09f106b2afdd287c30b2943
Ranchonyx <felixjanetzki@gmail.com>
2023-02-14 15:38:10 +0100

    minor changes

--- wasMint/wasMint.js
+++ wasMint/wasMint.js
@@ -76,6 +76,142 @@ class wasMintModule {
   };
   #free = () => {};
 
+  #typeConversionTable = {
+    Byte: {
+        read: (ptr, len = 0) => {
+            return new DataView(this.memory.buffer).getUint8(ptr, true);
+        },
+        size: 1,
+    },
+    Word: {
+        read: (ptr, len = 0) => {
+            return new DataView(this.memory.buffer).getUint16(ptr, true);
+        },
+        size: 2,
+    },
+    Dword: {
+        read: (ptr, len = 0) => {
+            return new DataView(this.memory.buffer).getUint32(ptr, true);
+        },
+        size: 4,
+    },
+    Qword: {
+        read: (ptr, len = 0) => {
+            return new DataView(this.memory.buffer).getBigUint64(ptr, true);
+        },
+        size: 8,
+    },
+    Int8Array: {
+        read: (ptr, len) => {
+            return new Int8Array(this.memory.buffer.slice(ptr, ptr + length * 1));
+        },
+    },
+    Uint8Array: {
+        read: (ptr, len) => {
+            return new Uint8Array(this.memory.buffer.slice(ptr, ptr + len * 1));
+        },
+    },
+    Uint8ClampedArray: {
+        read: (ptr, len) => {
+            return new Uint8ClampedArray(
+                this.memory.buffer.slice(ptr, ptr + len * 1)
+            );
+        },
+    },
+    Int16Array: {
+        read: (ptr, len) => {
+            return new Int16Array(this.memory.buffer.slice(ptr, ptr + len * 2));
+        },
+    },
+    Uint16Array: {
+        read: (ptr, len) => {
+            return new Uint16Array(this.memory.buffer.slice(ptr, ptr + len * 2));
+        },
+    },
+    Int32Array: {
+        read: (ptr, len) => {
+            return new Int32Array(this.memory.buffer.slice(ptr, ptr + len * 4));
+        },
+    },
+    Uint32Array: {
+        read: (ptr, len) => {
+            return new Uint32Array(this.memory.buffer.slice(ptr, ptr + len * 4));
+        },
+    },
+    Float32Array: {
+        read: (ptr, len) => {
+            return new Float32Array(this.memory.buffer.slice(ptr, ptr + len * 4));
+        },
+    },
+    Float64Array: {
+        read: (ptr, len) => {
+            return new Float64Array(this.memory.buffer.slice(ptr, ptr + len * 8));
+        },
+    },
+    BigInt64Array: {
+        read: (ptr, len) => {
+            return new BigInt64Array(this.memory.buffer.slice(ptr, ptr + len * 8));
+        },
+    },
+    BigUint64Array: {
+        read: (ptr, len) => {
+            return new BigUint64Array(this.memory.buffer.slice(ptr, ptr + len * 8));
+        },
+    },
+    Struct: {
+        read: (ptr, len = 0) => {
+            //Read data and increment data_bp according to their sizes
+            let bp = ptr;
+            let tmpObject = {};
+            for (let ft of this.globals[global.name].meta.fields.wasm) {
+                if (ft === "String") {
+                    tmpObject[`${ft}+*${bp}`] = readTable[ft].read(bp);
+                } else {
+                    tmpObject[`${ft}+${bp}`] = readTable[ft].read(bp);
+                }
+                data_bp += readTable[ft].size;
+            }
+            return tmpObject;
+        },
+    },
+    Number: {
+        read: (ptr, len = 0) => {
+            return new Number(ptr);
+        }
+    },
+    String: {
+        read: (ptr, len = 0, fromFunction = false) => {
+            return fromFunction ? this.#wasMintPtrToString() : this.#wasMintPtrToString(this.peekp(ptr));
+        },
+        size: 4,
+    },
+    BigInt: {
+        read: (ptr, len = 0) => {
+            return BigInt(ptr);
+        }
+    },
+    Undefined: {
+        read: (ptr, len = 0) => {
+            return undefined;
+        }
+    }
+};
+
+#typedArrayCtorTable = {
+  Int8Array: Int8Array,
+  Uint8Array: Uint8Array,
+  Uint8ClampedArray: Uint8ClampedArray,
+  Int16Array: Int16Array,
+  Uint16Array: Uint16Array,
+  Int32Array: Int32Array,
+  Uint32Array: Uint32Array,
+  Float32Array: Float32Array,
+  Float64Array: Float64Array,
+  BigInt64Array: BigInt64Array,
+  BigUint64Array: BigUint64Array,
+};
+
+
   constructor(
     wasmPath,
     moduleConfig,
@@ -96,20 +232,6 @@ class wasMintModule {
     if (!("WebAssembly" in window)) {
       alert("you need a browser with wasm support enabled :(");
     }
-
-    this.typedArrCtors = {
-      Int8Array: Int8Array,
-      Uint8Array: Uint8Array,
-      Uint8ClampedArray: Uint8ClampedArray,
-      Int16Array: Int16Array,
-      Uint16Array: Uint16Array,
-      Int32Array: Int32Array,
-      Uint32Array: Uint32Array,
-      Float32Array: Float32Array,
-      Float64Array: Float64Array,
-      BigInt64Array: BigInt64Array,
-      BigUint64Array: BigUint64Array,
-    };
 
     let tmpGlobals = {};
     let tmpFunctions = {};
@@ -377,6 +499,7 @@ class wasMintModule {
               let primaryResult = __functions__[funKey]._function(...finalArgs);
 
               let castResult = (ptr, len) => {
+                return this.#typeConversionTable[__functions__[funKey].return.type].read(ptr, len);
                 switch (__functions__[funKey].return.type) {
                   case "Number":
                     return new Number(ptr);
@@ -447,7 +570,7 @@ class wasMintModule {
                   default:
                     return ptr;
                     break;
-                }
+               }
               };
 
               //Let castResult decide what should be returned
@@ -627,7 +750,7 @@ class wasMintModule {
   #wasMintArrayToPtr(array, type) {
     let ptr = this.#malloc(array.byteLength);
 
-    let buffer = new this.typedArrCtors[type](
+    let buffer = new this.#typedArrayCtorTable[type](
       this.memory.buffer,
       ptr,
       array.length,
@@ -714,116 +837,7 @@ class wasMintModule {
   };
 
   readGlobal = (global) => {
-    return {
-      Byte: {
-        read: (ptr, len = 0) => {
-          return new DataView(this.memory.buffer).getUint8(ptr, true);
-        },
-        size: 1,
-      },
-      Word: {
-        read: (ptr, len = 0) => {
-          return new DataView(this.memory.buffer).getUint16(ptr, true);
-        },
-        size: 2,
-      },
-      Dword: {
-        read: (ptr, len = 0) => {
-          return new DataView(this.memory.buffer).getUint32(ptr, true);
-        },
-        size: 4,
-      },
-      Qword: {
-        read: (ptr, len = 0) => {
-          //Align might be wrong, gotta test!!!
-          return new DataView(this.memory.buffer).getBigUint64(ptr, true);
-        },
-        size: 8,
-      },
-      String: {
-        read: (ptr, len = 0) => {
-          return this.#wasMintPtrToString(this.peekp(ptr));
-        },
-        size: 4,
-      },
-      Int8Array: {
-        read: (ptr, len) => {
-          return new Int8Array(this.memory.buffer.slice(ptr, ptr + length * 1));
-        },
-      },
-      Uint8Array: {
-        read: (ptr, len) => {
-          return new Uint8Array(this.memory.buffer.slice(ptr, ptr + len * 1));
-        },
-      },
-      Uint8ClampedArray: {
-        read: (ptr, len) => {
-          return new Uint8ClampedArray(
-            this.memory.buffer.slice(ptr, ptr + len * 1)
-          );
-        },
-      },
-      Int16Array: {
-        read: (ptr, len) => {
-          return new Int16Array(this.memory.buffer.slice(ptr, ptr + len * 2));
-        },
-      },
-      Uint16Array: {
-        read: (ptr, len) => {
-          return new Uint16Array(this.memory.buffer.slice(ptr, ptr + len * 2));
-        },
-      },
-      Int32Array: {
-        read: (ptr, len) => {
-          return new Int32Array(this.memory.buffer.slice(ptr, ptr + len * 4));
-        },
-      },
-      Uint32Array: {
-        read: (ptr, len) => {
-          return new Uint32Array(this.memory.buffer.slice(ptr, ptr + len * 4));
-        },
-      },
-      Float32Array: {
-        read: (ptr, len) => {
-          return new Float32Array(this.memory.buffer.slice(ptr, ptr + len * 4));
-        },
-      },
-      Float64Array: {
-        read: (ptr, len) => {
-          return new Float64Array(this.memory.buffer.slice(ptr, ptr + len * 8));
-        },
-      },
-      BigInt64Array: {
-        read: (ptr, len) => {
-          return new BigInt64Array(
-            this.memory.buffer.slice(ptr, ptr + len * 8)
-          );
-        },
-      },
-      BigUint64Array: {
-        read: (ptr, len) => {
-          return new BigUint64Array(
-            this.memory.buffer.slice(ptr, ptr + len * 8)
-          );
-        },
-      },
-      Struct: {
-        read: (ptr, len = 0) => {
-          //Read data and increment data_bp according to their sizes
-          let bp = ptr;
-          let tmpObject = {};
-          for (let ft of this.globals[global.name].meta.fields.wasm) {
-            if (ft === "String") {
-              tmpObject[`${ft}+*${bp}`] = readTable[ft].read(bp);
-            } else {
-              tmpObject[`${ft}+${bp}`] = readTable[ft].read(bp);
-            }
-            data_bp += readTable[ft].size;
-          }
-          return tmpObject;
-        },
-      },
-    }[this.globals[global.name].meta.type].read(global.ptr, global.meta.length || 0);
+    return [this.globals[global.name].meta.type].read(global.ptr, global.meta.length || 0);
   }
 }
 
